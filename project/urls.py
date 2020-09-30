310a21208e41020884ebcbf7dae18de9ab956019
NoiTooo <t.komenoi@gmail.com>
2020-10-01 01:36:00 +0900

    repare robots

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .sitemaps import (
     ArticleSitemap,
@@ -26,11 +27,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('account/', include('register.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    # article/templates内にrobots.txtが入っている
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-        ] + urlpatterns
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
